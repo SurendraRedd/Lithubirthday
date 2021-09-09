5c81992b9df1313056cbb24ba10c6316a7f95a43
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2021-09-10 03:26:01 +0530

    Modified the file

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from streamlit_timeline import timeline
 # use full page width
 st.set_page_config(page_title="BirthDay Wishes", layout="wide")
 
+# Test/Title
+st.image('https://github.com/SurendraRedd/Lithubirthday/blob/main/IMG-20210408-WA0006.jpg?raw=true',width=250)
+st.markdown('# 🎂🎂🎂🎂🎂 Happy 4th Birthday Lithu')
+
+
 # load data
 with open('example.json', "r") as f:
     data = f.read()
